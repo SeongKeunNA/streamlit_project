ffa8fd061c5e51a610e1e3f78784a231883d2cc8
sihyeong671 <bshlab671@naver.com>
2022-12-17 00:16:28 +0900

    add function docstring

--- pages/1_📊Data Viewer.py
+++ pages/1_📊Data Viewer.py
@@ -1,17 +1,40 @@
 import streamlit as st
 import os
+import cv2
+from math import ceil
+from time import time
 
-from utils.data import get_data_folders, get_img_paths
+from utils.data import *
 
 ROOTDIR = "data"
+ELEMENTS_PER_PAGE = 10
 
 st.set_page_config(page_title="Data Viewer", page_icon="📊")
+set_session()
 
 st.title("Data Viewer")
 
 with st.sidebar:
+    
     selected_dir = st.selectbox(
         label="select folder", options=get_data_folders(ROOTDIR)
     )
+    
     img_paths = get_img_paths(os.path.join(ROOTDIR, selected_dir))
-    st.radio(label="사진 선택",options=img_paths[:10])
+    
+    img_path = st.radio(
+        label="사진 선택",
+        options=get_current_page_list(img_paths, st.session_state.page, ELEMENTS_PER_PAGE),
+        format_func=lambda x: f"{x.split('/')[-1]}")
+    
+    page2move = st.slider(label='페이지를 선택하세요', min_value=1, max_value=ceil(len(img_paths)/ELEMENTS_PER_PAGE))
+    
+    st.button(label='이동', on_click=move_page, args=([page2move - 1]))
+
+start_time = time()
+img = cv2.imread(img_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+show_img(img)
+end_time = time()
+
+st.write(f"loading time: {end_time - start_time:.3f}")
--- utils/data.py
+++ utils/data.py
@@ -7,7 +7,6 @@ import streamlit as st
 from pycocotools.coco import COCO
 
 
-
 def get_data_folders(dir_path: str) -> list:
     """root 하위 폴더 리스트 반환
 
@@ -25,8 +24,9 @@ def show_img(img: np.ndarray) -> None:
         img (np.ndarray): numpy image
     """
     st.image(img)
+    
 
-
+@st.cache()
 def get_img_paths(coco_path: str) -> list[str]:
     """img path 반환
 
@@ -42,4 +42,44 @@ def get_img_paths(coco_path: str) -> list[str]:
     for img_info in data.loadImgs(data.getImgIds()):
         img_paths.append(os.path.join(coco_path, 'images', img_info['file_name']))
 
-    return img_paths
+    return sorted(img_paths)
+
+
+def get_current_page_list(img_paths: list, page: int = 0, ele_per_page: int = 10) -> list[str]:
+    """현재 페이지에 해당하는 img path들을 반환
+
+    Args:
+        img_paths (list): 모든 img path가 있는 list
+        page (int, optional): 현재 페이지. Defaults to 0.
+        ele_per_page (int, optional): 페이지당 보여줄 이미지 경로의 갯수. Defaults to 10.
+
+    Returns:
+        list[str]: 페이지에 해당하는 이미지 경로가 들어있는 리스트
+    """
+    first_idx = page*ele_per_page
+    last_idx = (page+1)*ele_per_page
+    page_list = img_paths[first_idx:last_idx]
+    return page_list
+    
+
+def set_session():
+    """data app에서 세션 설정
+    """
+    if 'page' not in st.session_state:
+        st.session_state.page = 0
+        
+        
+def move_page(page: int):
+    """원하는 페이지로 이동
+
+    Args:
+        page (int): 이동을 원하는 페이지 번호
+
+    Raises:
+        ValueError: page error있는 경우 발생
+    """
+    try:
+        st.session_state.page = page
+    except:
+        raise ValueError('page에 int가 아닌 값이 들어왔거나 page변수가 존재하지 않습니다.')
+    
